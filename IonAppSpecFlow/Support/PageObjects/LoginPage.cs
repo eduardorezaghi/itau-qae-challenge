baea2fcc8d53a024ab928b865c9554267370094b
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2024-09-21 13:11:00 -0300

    Add LoginPage POM class

---
+++ IonAppSpecFlow/Support/PageObjects/LoginPage.cs
@@ -0,0 +1,29 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Android.UiAutomator;
+using OpenQA.Selenium.Support.Extensions;
+using OpenQA.Selenium.Appium.Interfaces;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+
+
+namespace IonAppSpecFlow.StepDefinitions
+{
+    public class LoginPage : BasePage
+    {
+        private readonly By pageTitle = MobileBy.Id("com.itau.investimentos:id/titleTextView");
+
+        public LoginPage(AndroidDriver driver) : base(driver) { }
+
+        public bool IsScreenDisplayed()
+        {
+            IsElementDisplayed(pageTitle);
+            string titleText = driver.FindElement(pageTitle).Text;
+            Assert.IsTrue(Regex.IsMatch(titleText, "acesso à conta", RegexOptions.IgnoreCase));
+
+            return true;
+        }
+    }
+}
