3254c58a4fd0831507a24161c9bf39e7a6786448
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2024-09-21 16:19:32 -0300

    Refactor BasePage to use FluentWait with timeout/polling constants
     - Use FluentWait to improve waiting for elements

--- IonAppSpecFlow/Support/PageObjects/BasePage.cs
+++ IonAppSpecFlow/Support/PageObjects/BasePage.cs
@@ -10,22 +10,37 @@ namespace IonAppSpecFlow.StepDefinitions
     {
         protected readonly AndroidDriver driver;
         protected readonly WebDriverWait driverWait;
+        private readonly int DEFAULT_TIMEOUT = 10;
+        private readonly int POLLING_INTERVAL = 250;
 
         public BasePage(AndroidDriver driver)
         {
             this.driver = driver;
-            this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            this.driverWait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(DEFAULT_TIMEOUT);
+            this.driverWait = new WebDriverWait(driver, TimeSpan.FromSeconds(DEFAULT_TIMEOUT));
         }
 
         protected void ClickElement(By by)
         {
-            driver.FindElement(by).Click();
+            WaitForElement(by).Click();
+        }
+
+        protected IWebElement WaitForElement(By by)
+        {
+            var fluentWait = new DefaultWait<AndroidDriver>(driver)
+            {
+                Timeout = TimeSpan.FromSeconds(DEFAULT_TIMEOUT),
+                PollingInterval = TimeSpan.FromMilliseconds(POLLING_INTERVAL)
+            };
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException), typeof(ElementNotVisibleException));
+            IWebElement element = fluentWait.Until(driver => driver.FindElement(by));
+
+            return element;
         }
 
         protected string WriteText(By by, string text)
         {
-            WebElement element = driver.FindElement(by);
+            WebElement element = (WebElement)WaitForElement(by);
             var oldText = element.Text;
             element.Clear();
             element.SendKeys(text);
@@ -36,14 +51,14 @@ namespace IonAppSpecFlow.StepDefinitions
 
         protected bool AssertText(By by, string expectedText)
         {
-            string actualText = driver.FindElement(by).Text;
+            string actualText = WaitForElement(by).Text;
             Assert.AreEqual(expectedText, actualText);
             return true;
         }
 
         protected bool AssertText(By by, Regex expectedText)
         {
-            string actualText = driver.FindElement(by).Text;
+            string actualText = WaitForElement(by).Text;
             Assert.IsTrue(expectedText.IsMatch(actualText));
             return true;
         }
