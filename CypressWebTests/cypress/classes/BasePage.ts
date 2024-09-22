0bc76fe167236f0c745b2be0607531382cb2647c
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2024-09-21 21:06:23 -0300

    Add visitPage on BasePage for generic usage

--- CypressWebTests/cypress/classes/BasePage.ts
+++ CypressWebTests/cypress/classes/BasePage.ts
@@ -1,4 +1,8 @@
 export class BasePage {
+    public visitPage(url: string): void {
+        cy.visit(url);
+    }
+
     protected getElement(selector: string): Cypress.Chainable<JQuery<HTMLElement>> {
         return cy.get(selector);
     }
